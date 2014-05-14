4219c636cbab60c9fb426b1086bd0a6822a5cc93
Nick Walker <nwalker75@yahoo.com>
2014-05-14 14:02:48 -0600

    first commit. practice writing functions.

---
+++ main.js
@@ -0,0 +1,15 @@
+// $(document).on('ready', function() {
+  
+// });
+
+var letterCapitalize = function(str) {
+	var wordSplit = str.split(' ');
+		// for (i=0; i<str.length; i++)
+			// str.charAt(0).toUpperCase() + str.slice(1);
+
+	// var newString = wordSplit 
+};
+
+console.log(letterCapitalize('make this work like a charm.'))
+
+// console.log(wordSplit)
